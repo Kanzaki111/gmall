a3bc338980cff6990950ce0b6d178d7c92f8b274
Kanzaki111 <1846317120@qq.com>
2019-12-25 15:44:51 +0800

    测试版本1

---
+++ demo/src/test/java/com/gec/demo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.gec.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/dotest")
+    public String doTest(){
+        return "hello world";
+    }
+}
